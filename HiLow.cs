7a328fa02f9e53c64830e11a0a02fde7b9bf1b0d
Brendan Hellar and Zema Kebede <zema@>
2019-04-22 13:34:37 -0700

    Halve the guessed number

--- HiLow.cs
+++ HiLow.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 
   public class NumberWizard
   {
@@ -8,7 +8,7 @@ using System;
 
       int max = 1000;
       int min = 1;
-      int guess = 500;
+      // int guess = 500;
       // Start is called before the first frame update
 
       Console.WriteLine("Welcome to Wizard");
@@ -17,31 +17,35 @@ using System;
       Console.WriteLine("The lowest number you can pick is: " + min);
       Console.WriteLine("Tell me if your number is higher or lower than 500");
       Console.WriteLine("Type 'h' for 'Higher', Type 'L' for Lower, type 'C' for 'Correct'");
-      max = max + 1;
-    // }
-      // Update is called once per frame
-    // public static void Keys()
-    // {
-        string key = Console.ReadKey().ToString();
 
-        if (key == "h" || key == "H")
-        {
-            Console.WriteLine("Higher!");
-            min = guess;
-            guess = (max + min) / 2;
-            Console.WriteLine(guess);
-        }
-        else if (key == "l" || key == "L")
-        {
-            Console.WriteLine("Lower!");
-            max = guess;
-            guess = (max + min) / 2;
-            Console.WriteLine(guess);
-        }
-        else if (key == "c" || key == "C")
-        {
-            Console.WriteLine("I guess your number!");
-        }
-    // }
+      Loop();
   }
+       public static void Loop(){
+         List<int> guessNumber = new List<int> {};
+         int guess = 500;
+
+         string key = Console.ReadLine();
+          if (key == "h" || key == "H")
+          {
+              Console.WriteLine("Higher!");
+              guessNumber.Add(guess);
+              Console.WriteLine(guessNumber[0]);
+              guessNumber[0] = (guessNumber[0])/2;
+              Console.WriteLine(guessNumber[0]);
+
+              // Loop();
+          }
+          else if (key == "l" || key == "L")
+          {
+              Console.WriteLine("Lower!");
+
+              guess = guess / 2;
+              Console.WriteLine(guess);
+              // Loop();
+          }
+          else if (key == "c" || key == "C")
+          {
+              Console.WriteLine("I guess your number!");
+          }
+        }
 }
